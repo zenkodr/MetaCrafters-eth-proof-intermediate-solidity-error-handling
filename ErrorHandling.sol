53e31e83f38ee40f18b19a925951f925d19a7de6
Joji Martinez <73366769+zenkodr@users.noreply.github.com>
2023-09-07 07:44:52 -0700

    Update ErrorHandling.sol

--- ErrorHandling.sol
+++ ErrorHandling.sol
@@ -7,16 +7,18 @@ contract ErrorHandling {
     constructor() {
         num = 0;
     }
-
+    //  check if the input _num is greater than 10
     function testRequire(uint _num) public {
         require(_num > 10, "Input number must be greater than 10");
         num = _num;
     }
 
+    // asserting that num is greater than 10
     function testAssert() public view {
         assert(num > 10);
     }
 
+    // manually reverting the transaction with custom error  refunding unused gas
     function testRevert() public pure {
         if (false) {
             revert("Error occurred");
